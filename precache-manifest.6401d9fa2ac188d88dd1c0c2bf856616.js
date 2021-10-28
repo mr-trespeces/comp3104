055f77a533b5b86ae4c18eb704edfd5dd98097f2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-10-28 17:04:24 +0000

    Deploy mr-trespeces/comp3104 to github.com/mr-trespeces/comp3104.git:gh-pages

---
+++ precache-manifest.6401d9fa2ac188d88dd1c0c2bf856616.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9abc6bfd636509d26a81",
+    "url": "/mr-trespeces/comp3104/static/css/main.ebf8f99c.chunk.css"
+  },
+  {
+    "revision": "9abc6bfd636509d26a81",
+    "url": "/mr-trespeces/comp3104/static/js/main.9abc6bfd.chunk.js"
+  },
+  {
+    "revision": "9f09623a0d611314a766",
+    "url": "/mr-trespeces/comp3104/static/js/runtime~main.9f09623a.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/mr-trespeces/comp3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "54624dc6ced75065378641065e97b133",
+    "url": "/mr-trespeces/comp3104/index.html"
+  }
+];
